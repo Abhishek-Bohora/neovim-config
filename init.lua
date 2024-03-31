54b2f3dca83acddbe80bcfc0d5018e5a2a0f89fe
Abhishek-Bohora <abhishek.bohora16@gmail.com>
2024-03-31 20:58:38 +0545

    added vim-options

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -17,10 +12,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
-local opts = {}
 
-require("lazy").setup(plugins, opts)
+require("vim-options")
+require("lazy").setup("plugins")
 
 
 
