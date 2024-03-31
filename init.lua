df9680612f765c179d0b90a65d37ec94bf5af34d
Abhishek-Bohora <abhishek.bohora16@gmail.com>
2024-03-31 18:29:48 +0545

    neo-tree plugin

--- init.lua
+++ init.lua
@@ -25,7 +25,17 @@ local plugins = {
 -- or                              , branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  }
 }
 
 local opts = {}
@@ -34,6 +44,7 @@ require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n','<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>h', ':Neotree filesystem reveal left<CR>', {})
 
 local config = require("nvim-treesitter.configs")
 config.setup({
