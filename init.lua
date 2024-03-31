7b8eddb8cff73bcfa8b42eaf7fa26a03c0ff0ebb
Abhishek-Bohora <abhishek.bohora16@gmail.com>
2024-03-31 19:06:00 +0545

    breaking config into own module

--- init.lua
+++ init.lua
@@ -15,29 +15,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
-
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  -- init.lua:
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
--- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  }
-}
-
+require("lazy").setup("plugins")
 local opts = {}
 
 require("lazy").setup(plugins, opts)
@@ -53,6 +33,4 @@ config.setup({
   indent = {enable = true}
 })
 
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
 
