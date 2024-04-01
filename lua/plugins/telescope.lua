e8c646d2d94988cfa17134263c77be1c093f0f07
Abhishek-Bohora <abhishek.bohora16@gmail.com>
2024-04-01 11:59:28 +0545

    added none-ls for formatting and linting

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,11 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '-1.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set('n','<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
-    end
+	"nvim-telescope/telescope.nvim",
+	tag = "-1.1.6",
+	dependencies = { "nvim-lua/plenary.nvim" },
+
+	config = function()
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+		vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
+	end,
 }
